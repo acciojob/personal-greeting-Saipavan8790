ee1cf9ced44f20796a70f784cfe2215b036f5996
Saipavan8790 <127079433+Saipavan8790@users.noreply.github.com>
2023-12-12 05:23:14 +0000

    first release

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [input, setInput] = useState("");
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <label for="name">Enter your name:</label>
+        <input type="text" onChange = {(event) => setInput(event.target.value)} name="name" required></input>
+        {input ? <p>Hello {input}!</p> : ""}
     </div>
   )
 }
